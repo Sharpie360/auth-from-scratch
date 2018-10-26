b33ef6b8b52a6b0265fe80feb8703790fa04faa3
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-25 23:39:15 -0500

    updated - request params check against db

--- auth/index.js
+++ auth/index.js
@@ -1,8 +1,17 @@
+// critical requires
 const express = require('express')
-const router = express.Router()
 const Joi = require('joi')
+const bcrypt = require('bcryptjs')
 
+// db
+const db = require('../db/connection')
+const users = db.get('users')
+users.createIndex('username', { unique: true })
 
+// init Express Router
+const router = express.Router()
+
+// Validation Schema
 const schema = Joi.object().keys({
   username: Joi.string().regex(/(^[a-zA-Z0-9_]+$)/).min(3).max(30).required(),
   password: Joi.string().min(10).required()
@@ -20,10 +29,33 @@ router.get('/', (req, res) => {
 
 // POST/auth/signup
 
-router.post('/signup', (req, res) => {
+router.post('/signup', (req, res, next) => {
   console.log('body', req.body)
+  // validate the request body
   const result = Joi.validate(req.body, schema)
-  res.json(result)
+  // if NO error, search for existing credentials : user
+  if(result.error === null){
+    // monk findOne
+    users.findOne({
+      username: req.body.username
+    }).then(user => {
+      // if user returns null, user does not exist
+      // otherwise throw error for duplicate
+      if(user){
+        // this user name already exists in db
+        const error = new Error('That username is already claimed. Please choose another one.')
+        next(error)
+      } else {
+        // hash the password
+        // insert the user witht he hashed password
+
+      }
+      res.json({ user })
+    })
+  } else {
+    // throw error from express
+    next(result.error)
+  }
 })
 
 
