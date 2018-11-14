a034b74c23bbfb7d6bf6736ff6ddfff6343e1f2c
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-13 23:28:14 -0600

    updated - POST request save note data to DB

--- api/notes.js
+++ api/notes.js
@@ -1,14 +1,47 @@
-const express = require("express");
+const express = require('express');
+const Joi = require('joi');
+
+const db = require('../db/connection');
+const notes = db.get('notes');
+
+// Validation Schema
+const schema = Joi.object().keys({
+  title: Joi.string()
+    .trim()
+    .min(3)
+    .max(100)
+    .required(),
+  body: Joi.string()
+    .trim()
+    .required()
+});
 
 const router = express.Router();
 
 // GET api/v1/notes
 router.get('/', (req, res) => {
-  res.json([])
-})
-
-
+  res.json([]);
+});
+// POST api/v1/notes
+router.post('/', (req, res, next) => {
+  // validate note
+  const result = Joi.validate(req.body, schema)
+  if(result.error === null){
+    // insert note into DB
+    const note = {
+      ...req.body,
+      user_id: req.user._id
+    }
+    notes.insert(note).then(insertedNote => {
+      res.json(insertedNote)
+    })
+  } else {
+    const error = new Error(result.error)
+    res.status(422)
+    next(error)
+  }
+});
 
 module.exports = {
   router
-}
+};
